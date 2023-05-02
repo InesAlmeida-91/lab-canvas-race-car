313b0799cf8c460a01769485548cd569d0afb8ee
Inês Almeida <ines_almeida_203@hotmail.com>
2023-05-02 14:36:20 +0100

    done just iteration 1

--- js/index.js
+++ js/index.js
@@ -3,5 +3,25 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
-};
+  function startGame() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    //clear canvas
+    ctx.clearRect(0, 0, 500, 700);
+    // Load road image
+    const roadImg = new Image(); // Create new <img> element
+    roadImg.src = './images/road.png'; // Set source path
+    // Draw road image on canvas drawImage(image, x, y, width, height)
+    ctx.drawImage(roadImg, 0, 0, 500, 700);  
+  } //only appears after 2 clicks??
+
+  const carImg = new player()
+  carImg = './images/car.png'
+  let carImgX = 0;
+  let carImgY = 0;
+  ctx.drawImage(carImg, carImgX, carImgY, 50, 50);
+
+}
+
+
+ 
