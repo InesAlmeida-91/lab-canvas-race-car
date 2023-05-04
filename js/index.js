2dbfb14f700240bbe9587f12a642eced5142235f
Inês Almeida <ines_almeida_203@hotmail.com>
2023-05-04 11:46:47 +0100

    update some functions

--- js/index.js
+++ js/index.js
@@ -1,21 +1,45 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame(); 
-    const carPlayer = new car();
-    carPlayer.draw();
   };
 
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-  class car {
+//startGame - just the road and the car
+//create the road
+//create the car - done in class car
+//create a game area 
+//move the car (rigth/left)
+//create obstacles
+//move the background with the obstacles
+//check the colision between car and obstacles
+//add score
+
+
+function startGame() {
+  road();
+  carPlayer.draw();
+  requestAnimationFrame(updateCanvas);
+}
+
+function road() {
+  const roadImg = new Image(); // Create new <img> element
+  roadImg.src = './images/road.png'; // Set source path
+  // Draw road image on canvas drawImage(image, x, y, width, height)
+  ctx.drawImage(roadImg, 0, 0, 500, 700);
+}
+
+class car {
     constructor() {
       this.x = 225;//starting posisiton x
       this.y = 575;//starting posisiton y
       this.width = 50;//img size
       this.height = 100;// img size
-      this.img = new Image();
-      this.img.src = '../images/car.png'
+      const img = new Image();
+      this.img = img;
+      img.src = '../images/car.png'
+      this.speedX = 0;
     }
     draw() {
       ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
@@ -26,14 +50,24 @@ window.onload = () => {
     moveRight() {
       this.x += 25;
     }
+    newPos() { // updating the position of the car
+      this.x += this.speedX;
+    }
+    update() {
+      const ctx = updateCanvas()
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
   }
+
+const carPlayer = new car();
 
 function updateCanvas() {
-    ctx.clearRect(0, 0, 500, 700);
-    carPlayer.draw();
+    road();
+    carPlayer.newPos();//in the new position??
+    carPlayer.update();
   }
 
-  document.addEventListener('keydown', e => {
+document.addEventListener('keydown', e => {
     switch (e.keyCode) {
       case 37:
         carPlayer.moveLeft();
@@ -46,18 +80,15 @@ function updateCanvas() {
     }
     updateCanvas();
   });
+ 
 
-  
-  
-  function startGame() {
-    //clear canvas
-    ctx.clearRect(0, 0, 500, 700);
-    // Load road image
-    const roadImg = new Image(); // Create new <img> element
-    roadImg.src = './images/road.png'; // Set source path
-    // Draw road image on canvas drawImage(image, x, y, width, height)
-    ctx.drawImage(roadImg, 0, 0, 500, 700);
-    } //only appears after 2 clicks??
+
+
+
+const myObstacles = [];
+
+function updateGameArea() { // updating our Game
+  myGameArea.clear(); // clear Canvas
 }
 
 
