cc5c625f93721eee1af70e3957f91a9d99124ba3
Inês Almeida <ines_almeida_203@hotmail.com>
2023-05-16 18:18:42 +0100

    create obstacles

--- js/index.js
+++ js/index.js
@@ -1,66 +1,75 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    startGame(); //initialize the game area
+    startGame();
   };
 }
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
-//startGame - just the road and the car
-//create the road
-//create the car - done in class car
-//create a game area -updatecanvas??
-//move the car (rigth/left) - done in class car
-//create obstacles
-//move the background with the obstacles
-//check the colision between car and obstacles
-//add score
-
+let frames = 0;
 
 function startGame() {
   road();
   carPlayer.draw();
-  requestAnimationFrame(updateCanvas);
+  setInterval(updateCanvas,20);
 }
+
+
+const roadImg = new Image(); 
+roadImg.src = '../images/road.png'; 
 
 function road() {
-  const roadImg = new Image(); // Create new <img> element
-  roadImg.src = './images/road.png'; // Set source path
-  ctx.drawImage(roadImg, 0, 0, 500, 700);  // Draw road image on canvas drawImage(image, x, y, width, height)
+  ctx.drawImage(roadImg, 0, 0, 500, 700);
 }
 
-class car {
+
+class Car {
     constructor() {
-      this.x = 225;//starting posisiton x
-      this.y = 575;//starting posisiton y
-      this.width = 50;//img size
-      this.height = 100;// img size
+      this.x = 225;
+      this.y = 575;
+      this.width = 50;
+      this.height = 100;
       const img = new Image();
       this.img = img;
       img.src = '../images/car.png'
       this.speedX = 0;
     }
     draw() {
-      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
     moveLeft() {
-      return this.x;
+      if (this.x >= 0) {
+      this.x -= 10;
+      }
     }
     moveRight() {
-      return this.x + this.width;
+      if(this.x <= 430) {
+        this.x += 10;
+      }
     }
-    newPos() { // updating the position of the car
+    newPos() {
       this.x += this.speedX;
     }
     update() {
-      const ctx = updateCanvas()
       ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+    top() {
+      this.y;
+    }
+    leftSide() {
+      this.x;
+    }
+    rightSide() {
+      this.x + this.width;
+    }
+    crashWith(obstacle) {
+      return !(this.top() > obstacle.bottom() || this.rightSide() < obstacle.leftSide() || this.leftSide() > obstacle.rightSide());
     }
   }
 
-const carPlayer = new car();
+const carPlayer = new Car();
 
-document.addEventListener('keydown', e => {
+document.addEventListener('keydown', (e) => {
   switch (e.keyCode) {
     case 37:
       carPlayer.moveLeft();
@@ -72,13 +81,79 @@ document.addEventListener('keydown', e => {
   updateCanvas();
 });
 
-function updateCanvas() {//update the game
-    ctx.clear()
-    road();
-    carPlayer.newPos();//new position 
-    carPlayer.update();//update new postition
-    //update obstacles
-    //check gameover
+
+class Obstacles {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = 'red';
+    this.x = x;
+    this.y = y;
   }
 
-const myObstacles = [];
+  update() {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }  
+    bottom() {
+      this.y + this.height
+    }
+    leftSide() {
+      this.x;
+    }
+    rightSide() {
+      this.x + this.width;
+    }
+  }
+
+  const myObstacles = [];
+
+  function updateObstacles() {
+    frames += 1;
+    if(frames % 160 === 0) {
+      let x = Math.floor(Math.random() * canvas.width)  // random start place of the obstacles  
+      let minWidth = 100;
+      let maxWidth = 350;
+      let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+      let height = 30;    
+      myObstacles.push(new Obstacles(width, height, 'red', x, 0))
+    }
+    for(let i = 0; i < myObstacles.length; i++) {
+      myObstacles[i].y +=1;
+      myObstacles[i].update();
+    }
+  }
+
+  let points = 0;
+
+  function score() {
+    points = Math.floor(frames / 5);
+    ctx.font = '18px serif';
+    ctx.fillStyle = 'black';
+    ctx.fillText(`Score: ${points}`, 50, 50);
+  }
+
+let gameStatus = 'playing'
+
+  /*function gameOver() {
+      if (carPlayer.crashWith) {
+        gameStatus = 'over';
+        clearInterval(updateCanvas);
+        ctx.font = '30px serif';
+        ctx.fillStyle = 'red';
+        ctx.fillText(`Game Over!`, 170, 150);
+        ctx.fillStyle = 'white';
+        ctx.fillText(`Your Final Score: ${points}`, 130, 200);
+    }
+  }*/
+
+
+  function updateCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    road();
+    carPlayer.newPos();
+    carPlayer.update();
+    updateObstacles();
+    score();
+   // gameOver();
+  }
